c07ecee0484a19c18f8b423a5daf100a9523dfcd
Contra-vit <40683716+Contra-vit@users.noreply.github.com>
2022-08-04 00:34:09 +0500

    likeCount

--- src/Components/Profile/Myposts/Post/Myposts.js
+++ src/Components/Profile/Myposts/Post/Myposts.js
@@ -3,14 +3,22 @@ import b from  './Myposts.module.css'
 import Post from './Post';
 
 const Myposts = () => {
+    let textarea =React.createRef();
+    // let textPost = React.createRef();
+
+    function addPost (){
+        console.log(textarea.current.textContent)
+        // textPost.current.message = textarea.current.textContent;
+    }
+
     return (
         <div>
             
             <div>
                 My posts
                 <div>
-                    <textarea cols="30" rows="10"></textarea>
-                    <button>Add post</button>
+                    <textarea cols="30" rows="10" ref= {textarea}>yuyuy</textarea>
+                    <button onClick={addPost}>Add post</button>
                     <button>Remove</button>
 
                 </div>
@@ -18,7 +26,7 @@ const Myposts = () => {
             <div className={b.posts} >
               
               <Post message ='How are you?'/>
-              <Post/>
+              <Post />
               <Post/>
               <Post/>
 
